b7852bfd6f0047de3f3bd058447a5991c90ef1da
Yosshi999 <Yosshi999@users.noreply.github.com>
2022-04-12 20:15:44 +0900

    merge text2mecab_s to original text2mecab

--- src/text2mecab/text2mecab.c
+++ src/text2mecab/text2mecab.c
@@ -92,54 +92,7 @@ static int strtopcmp(const char *str, const char *pattern)
    }
 }
 
-void text2mecab(char *output, const char *input)
-{
-   int i, j;
-   const int length = strlen(input);
-   const char *str;
-   int index = 0;
-   int s, e = -1;
-
-   for (s = 0; s < length;) {
-      str = &input[s];
-      /* search */
-      for (i = 0; text2mecab_conv_list[i] != NULL; i += 2) {
-         e = strtopcmp(str, text2mecab_conv_list[i]);
-         if (e != -1)
-            break;
-      }
-      if (e != -1) {
-         /* convert */
-         s += e;
-         str = text2mecab_conv_list[i + 1];
-         for (j = 0; str[j] != '\0'; j++)
-            output[index++] = str[j];
-      } else if (text2mecab_control_range[0] <= str[0] && str[0] <= text2mecab_control_range[1]) {
-         /* control character */
-         s++;
-      } else {
-         /* multi byte character */
-         e = -1;
-         for (j = 0; text2mecab_kanji_range[j] > 0; j += 3) {
-            if (text2mecab_kanji_range[j + 1] <= str[0] && text2mecab_kanji_range[j + 2] >= str[0]) {
-               e = text2mecab_kanji_range[j];
-               break;
-            }
-         }
-         if (e > 0) {
-            for (j = 0; j < e; j++)
-               output[index++] = input[s++];
-         } else {
-            /* unknown */
-            fprintf(stderr, "WARNING: text2mecab() in text2mecab.c: Wrong character.\n");
-            s++;
-         }
-      }
-   }
-   output[index] = '\0';
-}
-
-errno_t text2mecab_s(char *output, size_t sizeOfOutput, const char *input)
+errno_t text2mecab(char *output, size_t sizeOfOutput, const char *input)
 {
    if (input == NULL || output == NULL || sizeOfOutput == 0)
       return EINVAL;
